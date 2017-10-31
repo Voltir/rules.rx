434f0ca462538637169f18964dc7c446214f8112
Nick Childers <nick.childers@example.com>
2017-10-31 14:28:13 -0700

    wip - streaming steps

--- aws/src/main/scala/rules/aws/emr/Step.scala
+++ aws/src/main/scala/rules/aws/emr/Step.scala
@@ -5,6 +5,7 @@ import com.amazonaws.services.elasticmapreduce.model.{
   HadoopJarStepConfig,
   StepConfig
 }
+import com.amazonaws.services.elasticmapreduce.util.StreamingStep
 import rules.aws.s3.S3Path
 
 trait Step {
@@ -34,7 +35,54 @@ case class SparkJarStep(
       .withArgs(args: _*)
 
     new StepConfig()
-      .withName(s"${stepName.value}")
+      .withName(stepName.value)
+      .withHadoopJarStep(cfg)
+      .withActionOnFailure(ActionOnFailure.CONTINUE)
+  }
+}
+
+case class EmrStreamingStep(
+    override val stepName: StepName,
+    mapper: S3Path,
+    reducer: Option[S3Path],
+    output: S3Path,
+    inputs: S3Path*
+) extends Step {
+
+  override def config: StepConfig = {
+    val wurt = new StreamingStep()
+      .withInputs(inputs.map(_.path): _*)
+      .withMapper(mapper.path)
+      .withOutput(output.path)
+
+    val zzz = reducer.map { r =>
+      wurt.withReducer(r.path)
+    } getOrElse wurt
+
+    val omgfu =
+      zzz.toHadoopJarStepConfig.withJar("/usr/lib/hadoop/hadoop-streaming.jar")
+
+    new StepConfig()
+      .withName(stepName.value)
+      .withHadoopJarStep(omgfu)
+      .withActionOnFailure(ActionOnFailure.CONTINUE)
+  }
+}
+
+case class EmrStreamingStep2(
+  override val stepName: StepName,
+  args: List[String]
+) extends Step {
+  override def config = {
+    val cfg = new HadoopJarStepConfig()
+      .withJar("command-runner.jar")
+      .withArgs(
+        "hadoop-streaming"
+      )
+      .withArgs(args: _*)
+
+    new StepConfig()
+      .withName(stepName.value)
       .withHadoopJarStep(cfg)
       .withActionOnFailure(ActionOnFailure.CONTINUE)
   }
